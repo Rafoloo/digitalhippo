8b7e8b3c992e84f056d8bb355c75012d21511c52
rafael <rafolomaster@gmail.com>
2025-06-05 08:35:45 -0300

    feat: Criação da página inicial

---
+++ src/lib/utils.ts
@@ -0,0 +1,26 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice  (
+  price: number | string,
+  options: {
+    currency?: 'BRL' | 'USD' | 'EUR'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'BRL', notation = 'compact' } = options;
+
+  const numericPrice = 
+    typeof price === 'string' ? parseFloat(price) : price
+
+    return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency,
+    notation,
+    minimumFractionDigits: 2,
+}).format(numericPrice)
+}
